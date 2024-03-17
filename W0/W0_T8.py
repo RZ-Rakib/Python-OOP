8ccf6cff46b54ebe0c625fc74467f2c85f142bf6
RZ Rakib <rakibzaman824@gmail.com>
2024-03-17 17:26:37 +0200

    made some changes with my files name on my local machine

---
+++ W0/W0_T8.py
@@ -0,0 +1,13 @@
+#import math faction and import pi and tau from math
+import math
+from math import pi, tau
+
+if pi == tau:
+    print("Tau equals Pi.")
+else:
+    print("Tau doesn't equals Pi.")
+
+if pi * 2 == tau:
+    print("Tau equals 2 * Pi.")
+else:
+    print("Tau doesn't equals to 2 * Pi.")
